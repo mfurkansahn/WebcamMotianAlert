40080e66b4bf9b6afb186fd3d139b05321f75a19
Mustafa Furkan ŞAHİN <mfurkansahn@hotmail.com>
2025-12-03 19:19:38 +0300

    send email when Webcam detects an object

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import glob
+import os
 
 import cv2
 import time
@@ -16,6 +17,12 @@ first_frame = None
 status_list = []
 
 count = 0
+
+
+def clean_folder():
+    images = glob.glob('images/*.jpg')
+    for image in images:
+        os.remove(image)
 
 while True:
     status = 0
@@ -58,7 +65,8 @@ while True:
     status_list = status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email()
+        send_email(image_with_object)
+        clean_folder()
 
     print(status_list)
 
